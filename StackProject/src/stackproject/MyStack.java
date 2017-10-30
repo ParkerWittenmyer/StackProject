cb5c98f1277b47a99bee34f03c42ec0cad8d912b
ParkerWittenmyer <31516692+ParkerWittenmyer@users.noreply.github.com>
2017-10-30 12:28:22 -0400

    Add files via upload

--- StackProject/src/stackproject/MyStack.java
+++ StackProject/src/stackproject/MyStack.java
@@ -11,20 +11,24 @@ import java.util.*;
  * @author pwittenmyer
  */
 public class MyStack<T> extends ArrayList<T>{
-    
+    private ArrayList<T> list1 = new ArrayList<>();
     
     public void Push(T o)
     {
-        add(o); //adds the generic 'o' item to the stack
+        list1.add(o); //adds the generic 'o' item to the stack
     }
-    
     public T Pop()
     {
-        return remove(size() - 1); //removes the next item from the stack
-    }
-    
-    public boolean IsEmpty()
-    {
-        return size()==0; //returns 0 if the stack is empty
+        if(list1.size() > 0)
+        {
+            T end = list1.get(list1.size()-1);
+            list1.remove(size() - 1); //removes the next item from the stack
+            return end;
+        }   
+        else
+        {
+            System.out.println("Error!");
+            return null;
+        }
     }
 }
--- StackProject/src/stackproject/StackProject.java
+++ StackProject/src/stackproject/StackProject.java
@@ -11,30 +11,20 @@ import java.util.*;
  *
  * @author pwittenmyer
  */
-public class StackProject<T> {
+public class StackProject {
 
     
     public static void main(String[] args) {
-        int i;
+        MyStack<Integer> stack = new MyStack<>(); //creates the stack
         
-        ArrayList<Integer> array1 = new ArrayList<>(); //creates the arraylist
         
-        MyStack stack2 = new MyStack(); //creates the stack
         
-        for(i = 1; i <= 10; i++) //initializes the for loop to add integers to the arraylist
-        {
-            array1.add(i); //adds integers to the arraylist
-        }
-        
-        stack2.Push(array1); //adds integers from the arraylist to the stack
-        
-        while (!stack2.IsEmpty()) //initializes the while loop for printing out the integers
-        {
-            System.out.println(stack2.Pop()); //prints out the integers
-            if(stack2.isEmpty() == true)
-            {
-                System.out.println("Done!"); //prints out 'Done!' when the stack is empty
-            }
-        }
+        stack.Push(3); 
+        stack.Push(2);
+        stack.Push(1);
+        System.out.println(stack.Pop());
+        //System.out.println(stack.Pop());
+        //System.out.println(stack.Pop());
+        //System.out.println(stack.Pop());
     }
 }
